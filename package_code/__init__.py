6ecd4bdc1875c0d5a17a99e15fffd8db6a9b135e
isavalenzuela <camivale@gmail.com>
2021-02-13 17:55:59 -0300

    update

--- package_code/__init__.py
+++ package_code/__init__.py
@@ -3,13 +3,17 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
+cred = credentials.Certificate('./vaccine-percentage-firebase-adminsdk-ogi68-3623de3267.json')
 
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://databaseName.firebaseio.com'
+    'databaseURL': 'https://vaccine-percentage-default-rtdb.firebaseio.com/'
 })
 
 # As an admin, the app has access to read and write all data, regradless of Security Rules
-ref = db.reference('restricted_access/secret_document')
-print(ref.get())
+ref = db.reference('countries')
+
+found_countries = ref.get()
+
+
+print(found_countries[0].name)
--- setup.py
+++ setup.py
@@ -6,14 +6,6 @@ try:
 except IOError:
     long_description = ""
 
-install_requires = [
-    'cachecontrol>=0.12.6',
-    'google-api-core[grpc] >= 1.14.0, < 2.0.0dev; platform.python_implementation != "PyPy"',
-    'google-api-python-client >= 1.7.8',
-    'google-cloud-firestore>=1.4.0; platform.python_implementation != "PyPy"',
-    'google-cloud-storage>=1.18.0',
-]
-
 setup(
     name="firebase-python-service",
     version="0.1.0",
@@ -21,11 +13,17 @@ setup(
     license="MIT",
     author="isavalenzuela",
     keywords='firebase cloud development',
-    install_requires=install_requires,
-    packages=['firebase_admin'],
+    install_requires=[
+        'cachecontrol>=0.12.6',
+        'google-api-core[grpc] >= 1.14.0, < 2.0.0dev; platform.python_implementation != "PyPy"',
+        'google-api-python-client >= 1.7.8',
+        'google-cloud-firestore>=1.4.0; platform.python_implementation != "PyPy"',
+        'google-cloud-storage>=1.18.0',
+    ],
+    packages=find_packages(),
     python_requires='>=3.5',
- classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
-    ]
+    classifiers=[
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.8",
+        ]
 )
